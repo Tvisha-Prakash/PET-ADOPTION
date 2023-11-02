731e75296363d0d709d54f3e8d0077a5028971b9
Srilaxmi Bhagawath <111692308+srilaxmibhagawath@users.noreply.github.com>
2023-11-02 20:29:42 +0530

    updated

--- login.php
+++ login.php
@@ -1,55 +1,39 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verify reCAPTCHA first
-    $recaptchaSecretKey = "6Lfe2dsoAAAAANptepBPd8j9gdDUeIn3opSOg3Sd";
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
+   
+    $email = $_POST["Email"];
+    $password = $_POST["Password"];
+    $captcha = $_POST["captcha"];
+    
+    $db = mysqli_connect("localhost", "root", "", "petadoption");
 
-    $url = 'https://www.google.com/recaptcha/api/siteverify';
-    $data = array(
-        'secret' => $recaptchaSecretKey,
-        'response' => $recaptchaResponse
-    );
-
-    $options = array(
-        'http' => array(
-            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method' => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    $responseKeys = json_decode($result, true);
-
-    if (intval($responseKeys["success"]) !== 1) {
-        // CAPTCHA verification failed, show an error message or redirect as needed
-        header("Location: login.html?captcha_error=1");
-        exit;
+    if (!$db) {
+        die("Connection failed: " . mysqli_connect_error());
     }
 
-    // If CAPTCHA verification is successful, proceed with login validation
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    // Create a PDO database connection
-    $db = new PDO("mysql:host=localhost;dbname=petadoption", "root", "");
-    
-    // Check the user's credentials against the database
-    $sql = "SELECT * FROM users WHERE email = :email";
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    if ($user && password_verify($password, $user["password"])) {
-        // Successful login
-        session_start();
-        $_SESSION["user_id"] = $user["id"]; // Store user ID in the session
-        header("Location: dashboard.html"); // Redirect to the user's dashboard
+    $checkSql = "SELECT Email, Password FROM registration WHERE Email = ?";
+    $checkStmt = mysqli_prepare($db, $checkSql);
+    mysqli_stmt_bind_param($checkStmt, "s", $email);
+    mysqli_stmt_execute($checkStmt);
+    mysqli_stmt_store_result($checkStmt);
+     
+    if (mysqli_stmt_num_rows($checkStmt) > 0) {
+        mysqli_stmt_bind_result($checkStmt, $dbEmail, $dbPassword);
+        mysqli_stmt_fetch($checkStmt);
+        $dbPassword = password_hash($dbPassword, PASSWORD_BCRYPT);
+      
+        if (password_verify($password, $dbPassword)) {
+          
+            header("Location: dashboard.html");
+            exit;
+        } else {
+            echo "Invalid password. Please try again.";
+        }
     } else {
-        // Invalid login, show an error message or redirect
-        header("Location: login.html?login_error=1");
+        echo "Email not found. Please register or try again.";
     }
+
+    mysqli_close($db);
 }
+
 ?>
--- register.php
+++ register.php
@@ -1,35 +1,29 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve user input
+   
+    $name = $_POST["name"];
     $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = password_hash($_POST["password"], PASSWORD_BCRYPT); // Hash the password
-    
-    // Store user information in the database (you'll need to set up your database connection)
-    // Example database connection (replace with your own details):
-    $db = new PDO("mysql:host=localhost;dbname=your_database", "your_username", "your_password");
-    
-    // Insert user data into the database
-    $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([$username, $email, $password]);
-    
-    // Redirect to a thank you page or login page
-    header("Location: thank_you.html");
-}
-$recaptchaSecretKey = "6Lfe2dsoAAAAANptepBPd8j9gdDUeIn3opSOg3Sd";
-$recaptchaResponse = $_POST["g-recaptcha-response"];
-$remoteIp = $_SERVER["REMOTE_ADDR"];
+    $mobile = $_POST["mobile"];
+    $email = $_POST["Email"];
+    $password = password_hash($_POST["Password"], PASSWORD_BCRYPT); // Hash the password
 
-$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptchaSecretKey&response=$recaptchaResponse&remoteip=$remoteIp");
-$responseKeys = json_decode($response, true);
+    
+    $db = new PDO("mysql:host=localhost;dbname=petadoption", "root", "");
 
-if (intval($responseKeys["success"]) !== 1) {
-    // CAPTCHA verification failed, handle accordingly
-    echo "CAPTCHA verification failed.";
-    // You may want to redirect back to the form with an error message.
-} else {
-    // CAPTCHA verification passed, continue with your registration or login process.
-    // Insert user data into the database or authenticate the user as needed.
+    $checkSql = "SELECT COUNT(*) FROM registration WHERE Email = ?";
+    $checkStmt = $db->prepare($checkSql);
+    $checkStmt->execute([$email]);
+    $count = $checkStmt->fetchColumn();
+
+    if ($count > 0) {
+
+        echo "Email already exists. Please use a different email.";
+    } else {
+        $sql = "INSERT INTO registration (Name, Username, Mobile, Email, Password) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$name, $username, $mobile, $email, $password]);
+
+        header("Location: thank_you.html");
+    }
 }
 ?>
