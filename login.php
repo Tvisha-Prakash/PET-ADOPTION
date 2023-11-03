bf44bffc673bec800642738915fce06296fc27ed
Writvan Ghosh <100478268+writvan@users.noreply.github.com>
2023-11-03 08:19:26 +0530

    Update login.php
    since pdo method is used for connecting in registration php it is better to use the same for login , i was wrong in the password hashing thing it need not be hashed again password_verify is enough try these changes hope it works

--- login.php
+++ login.php
@@ -3,27 +3,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
     $email = $_POST["Email"];
     $password = $_POST["Password"];
-    $captcha = $_POST["captcha"];
     
-    $db = mysqli_connect("localhost", "root", "", "petadoption");
-
-    if (!$db) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    $db = new PDO("mysql:host=localhost;dbname=petadoption", "root", "");
 
     $checkSql = "SELECT Email, Password FROM registration WHERE Email = ?";
-    $checkStmt = mysqli_prepare($db, $checkSql);
-    mysqli_stmt_bind_param($checkStmt, "s", $email);
-    mysqli_stmt_execute($checkStmt);
-    mysqli_stmt_store_result($checkStmt);
-     
-    if (mysqli_stmt_num_rows($checkStmt) > 0) {
-        mysqli_stmt_bind_result($checkStmt, $dbEmail, $dbPassword);
-        mysqli_stmt_fetch($checkStmt);
-        $dbPassword = password_hash($dbPassword, PASSWORD_BCRYPT);
+    $checkStmt = $db->prepare($checkSql);
+    $checkStmt->execute([$email]);
+    $result = $checkStmt->fetch(PDO::FETCH_ASSOC);
+    
+    if ($result) {
+        $dbPassword = $result["Password"];
       
         if (password_verify($password, $dbPassword)) {
-          
             header("Location: dashboard.html");
             exit;
         } else {
@@ -32,8 +23,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         echo "Email not found. Please register or try again.";
     }
-
-    mysqli_close($db);
 }
-
 ?>
